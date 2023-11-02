45b3836472109e04c4e5bc7fbeae5bf81ddc1f95
Chris Wangsanata <chriswangsanata@gmail.com>
2023-11-01 21:25:01 -0700

    Revert "change date formatting"
    This reverts commit 6b18a9f0b7df1d9d8f0f63789eb8d08a97911d13.

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,16 @@ const messages = [
   {
     text: "Welcome to the Wall",
     user: "System",
-    added: new Date()
+    added: new Date().toDateString()
   }
 ];
 
 router.get('/', function(req, res, next){
-  res.render('index', { title: "Wall (β)", messages: messages })
+  res.render('index', { title: "Wall", messages: messages })
 });
 
 router.post('/', function(req, res, next) {
-  messages.unshift({text: req.body.message, user: req.body.user, added: new Date()})
+  messages.push({text: req.body.message, user: req.body.user, added: new Date().toDateString()})
   res.redirect('/')
 });
 
