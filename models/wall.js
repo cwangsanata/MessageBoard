6f3c5df7ef0864e51d76b315b15165320443f109
Chris Wangsanata <chriswangsanata@gmail.com>
2023-11-20 14:56:30 -0800

    add unique name constraint

--- models/wall.js
+++ models/wall.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema
 const wallSchema = new Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     description: String,
     messages: {
