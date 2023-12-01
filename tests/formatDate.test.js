5c2385615ee73f6aa311a8d310a640f92f15cd34
Chris Wangsanata <chriswangsanata@gmail.com>
2023-11-30 22:33:25 -0800

    add brief description

--- tests/formatDate.test.js
+++ tests/formatDate.test.js
@@ -1,20 +1,21 @@
 const formatDate = require('../public/javascript/formatDate.js');
 
-test('Date is initally in ISO format', () => {
-    expect(new Date().toISOString()).toMatch(/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}/);
-});
+describe('formatDate', () => {
+    test('Date is initally in ISO format', () => {
+        expect(new Date().toISOString()).toMatch(/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}/);
+    });
 
-test ('formatDate returns a string', () => {
-    expect(typeof formatDate(new Date())).toBe('string');
-});
+    test ('formatDate returns a string', () => {
+        expect(typeof formatDate(new Date())).toBe('string');
+    });
 
-test ('formatDate returns a string in the correct format (fixed date)', () => {
-    const fixedDate = new Date('December 17, 1995 03:24:00');
-    expect(formatDate(fixedDate)).toBe('Dec 17, 1995, 3:24 AM');
-});
+    test ('formatDate returns a string in the correct format (fixed date)', () => {
+        const fixedDate = new Date('December 17, 1995 03:24:00');
+        expect(formatDate(fixedDate)).toBe('Dec 17, 1995, 3:24 AM');
+    });
 
-test ('formatDate returns a string in the correct format (current date)', () => {
-    const dateRegex = /\b(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s\d{1,2},\s\d{4},\s\d{1,2}:\d{2}\s(?:AM|PM)\b/;
-    expect(formatDate(new Date())).toMatch(dateRegex);
+    test ('formatDate returns a string in the correct format (current date)', () => {
+        const dateRegex = /\b(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s\d{1,2},\s\d{4},\s\d{1,2}:\d{2}\s(?:AM|PM)\b/;
+        expect(formatDate(new Date())).toMatch(dateRegex);
+    });
 });
-
